36d9ebc3221a9b4d42035756919b93e1d597a74e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-02-05 15:59:00 +0600

    show all details of a movie

--- app/[movie]/page.js
+++ app/[movie]/page.js
@@ -1,7 +1,29 @@
-export default async function MovieDetail() {
+import Image from "next/image";
+
+export default async function MovieDetail({ params }) {
+  const { movie } = params;
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+
   return (
     <div>
-      <h1>Movie Details</h1>
+      <h2 className="text-2xl">{res.title}</h2>
+      <h2>{res.release_date}</h2>
+      <h2>Runtime: {res.runtime} minutes</h2>
+      <h2 className="text-sm bg-green-600  text-white inline-block my-2 py-2 px-4 rounded-md">
+        {res.status}
+      </h2>
+      <Image
+        className="my-12 w-full"
+        src={imagePath + res.backdrop_path}
+        width={600}
+        height={600}
+        priority
+      />
+      <p>{res.overview}</p>
     </div>
-  )
+  );
 }
